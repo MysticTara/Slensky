f5cbc01c56e7ada756e8cc27810eb8f71bbde615
TeamThara <96575176+MysticTara@users.noreply.github.com>
2022-03-03 19:42:47 +0100

    Create cluster.js

---
+++ cluster.js
@@ -0,0 +1,21 @@
+Cluster.js 
+Import cluster@clarx
+const = clx@cluster
+const = discord.js = cluster version: 1.0.3
+
+# to get cluster clx you need to join our server (it's use to run the bot and it's only available only in our server)
+cluster: "cluster@import.llx://NeyoGolden:neji@webupt.vhkov.cluster.neyo./7297489:83=false&w=newclx"  
+clusterid: "7297489001853"
+
+const config = require(`config.json`);
+const {
+  Client,
+  Message,
+  MessageForm,
+  MessageSend,
+  MessageCluster4,
+  MessageButton
+} = require('cluster@clx.database.js');
+const eec = require(`embed.json`);
+
+
