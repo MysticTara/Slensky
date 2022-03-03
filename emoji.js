3f7c5934034d4b5b3e8ed41f2a01648e0a089bd6
TeamThara <96575176+MysticTara@users.noreply.github.com>
2022-03-03 20:04:24 +0100

    Create emoji.js

---
+++ emoji.js
@@ -0,0 +1,44 @@
+{
+  "y": "",
+  "m": "",
+  "x": "",
+  "music": {
+    "searching": "🔍",
+    "play": "",
+    "playImg": "",
+    "pause": "⏸️",
+    "resume": "▶️",
+    "skip": "⏭️",
+    "queue": "📑",
+    "stop": "⏹",
+    "volume": "🔊",
+    "loop": "🔂",
+    "autoplay": "🔁",
+    "shuffle": "🔀",
+    "addrelatedsong": "",
+    "less10Sec": "⏪",
+    "forward10Sec": "⏩",
+    "negative10vol": "🔉",
+    "plus10vol": "🔊"
+  },
+  "musicButtons": {
+    "pause": "⏸️",
+    "resume": "▶️",
+    "skip": "⏭️",
+    "queue": "📑",
+    "lyrics": "📝",
+    "stop": "⏹",
+    "loop": "🔂",
+    "autoplay": "🔁",
+    "shuffle": "🔀",
+    "less10Sec": "⏪",
+    "forward10Sec": "⏩",
+    "negative10vol": "🔉",
+    "plus10vol": "🔊"
+  },
+  "giveaway": {
+    "react": "",
+    "drop": ""
+  }
+}
+
